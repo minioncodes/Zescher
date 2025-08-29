c321d2a9f77c1d3da57d87e2edac3454c0859dbb
ryaaan4321 <aryan056400@gmail.com>
2025-08-29 18:02:14 +0530

    feat: implement user signup functionality with JWT authentication and MongoDB integration

---
+++ app/api/user/signup/route.ts
@@ -0,0 +1,50 @@
+import User from "@/models/User";
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt'
+import { NextResponse, NextRequest } from "next/server";
+import { cookies } from "next/headers";
+import connectDB from "@/lib/mongo";
+
+export async function POST(req: NextRequest, res: NextResponse) {
+    try {
+        await connectDB();
+        console.log("mongo uri = ",process.env.MONGO_URI)
+        const body = await req.json();
+        const { name, email, password, location, phoneNumber, dateOfBirth, address, gender } = body;
+        const cookieStore = await cookies();
+        const hashedPassword = await bcrypt.hashSync(body.password, 10);
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return NextResponse.json({ msg: "user alrady exist" }, { status: 400 });
+        }
+        const newUser = new User({
+            name,
+            email,
+            password: hashedPassword,
+            createdAt: new Date(),
+            location,
+            phoneNumber,
+            dateOfBirth,
+            address,
+            gender
+        });
+
+        await newUser.save();
+        if (!process.env.SECRET_KEY) throw new Error("SECRET_KEY is not defined");
+        const token = jwt.sign({ id: newUser._id, email: newUser.email }, process.env.SECRET_KEY, { expiresIn: "1h" });
+        cookieStore.set("token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV !== "development",
+            sameSite: "lax",
+            maxAge: 60 * 60,
+            path: "/",
+        });
+        console.log("response = ", newUser);
+        return NextResponse.json({ newUser, token }, { status: 201 })
+    } catch (e: any) {
+        console.log("message = ", e.message);
+        return NextResponse.json({ msg: e.message }, { status: 500 })
+    }
+}
+// Fzp2LPUna2CaDa8B
+// devs_db_user
--- models/User.ts
+++ models/User.ts
@@ -9,14 +9,21 @@ export interface IUser extends Document {
     password: string;
     createdAt: Date;
     location: string;
+    phoneNumber: string;
+    dateOfBirth: string;
+    address: string;
+    gender: string;
 }
-
 const UserSchema = new Schema<IUser>({
     _id: { type: String, default: uuidv4 },
     name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
     createdAt: { type: Date, required: true },
-    location: { type: String, required: true }
+    location: { type: String, required: true },
+    phoneNumber: { type: String },
+    dateOfBirth: { type: String },
+    address: { type: String },
+    gender: { type: String },
 })
 export default mongoose.models.User || mongoose.model<IUser>("User", UserSchema);

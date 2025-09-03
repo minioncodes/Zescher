1c2a92fdbe1e0ceed22f9eec87eed2fba1d55793
ryaaan4321 <aryan056400@gmail.com>
2025-09-03 18:21:17 +0530

    feat: enhance category and product models to use string for createdBy; update JWT verification in API routes

--- app/api/admin/product/create-product/route.ts
+++ app/api/admin/product/create-product/route.ts
@@ -3,31 +3,28 @@ import jwt from "jsonwebtoken";
 import Product from "@/models/admin/ProductSchema";
 import dbConnect from "@/lib/mongo";
 import cloudinary from "@/lib/cloudinary";
-
+import { AdminPayload } from "../../category/create-category/route";
+import { jwtVerify } from "jose";
 export async function POST(req: NextRequest) {
   try {
     await dbConnect();
-    const formdata=await req.formData();
-    const name=formdata.get("name");
-    const slug=formdata.get("slug");
-    const descreption=formdata.get("descreption")
-    const price=Number(formdata.get("price"));
-    const category=formdata.get("category")
-    const stock=formdata.get("stock")
-    const token = req.cookies.get("adminToken")?.value;
+    const token = await req.cookies.get("adminToken")?.value;
     if (!token) {
-      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+      return NextResponse.json({ msg: "Unauthorized" }, { status: 401 })
     }
-    let decoded: any;
-    try {
-      decoded = jwt.verify(token, process.env.SECRET_KEY as string);
-    } catch (err) {
-      return NextResponse.json({ message: "Invalid or expired token" }, { status: 401 });
-    }    
+    const { payload } = await jwtVerify<AdminPayload>(token, new TextEncoder().encode(process.env.SECRET_KEY));
+    const adminId = payload.id;
+    const formdata = await req.formData();
+    const name = formdata.get("name");
+    const slug = formdata.get("slug");
+    const description = formdata.get("description")
+    const price = Number(formdata.get("price"));
+    const category = formdata.get("category")
+    const stock = formdata.get("stock")
     const body = await req.json();
     const newProduct = await Product.create({
       ...body,
-      createdBy: decoded.id,
+      createdBy: adminId
     });
     return NextResponse.json(newProduct, { status: 201 });
   } catch (error) {

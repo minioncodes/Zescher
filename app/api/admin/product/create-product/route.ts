c1bd4be5056fa321572d64777ddead007ed1ca64
ryaaan4321 <aryan056400@gmail.com>
2025-09-04 16:31:06 +0530

    feat: implement product image upload to Cloudinary; refactor token retrieval and error handling in create-product route

--- app/api/admin/product/create-product/route.ts
+++ app/api/admin/product/create-product/route.ts
@@ -8,27 +8,25 @@ import { jwtVerify } from "jose";
 export async function POST(req: NextRequest) {
   try {
     await dbConnect();
-    const token = await req.cookies.get("adminToken")?.value;
+    const token = req.cookies.get("adminToken")?.value;
     if (!token) {
-      return NextResponse.json({ msg: "Unauthorized" }, { status: 401 })
+      return NextResponse.json({ msg: "Unauthorized" }, { status: 401 });
     }
-    const { payload } = await jwtVerify<AdminPayload>(token, new TextEncoder().encode(process.env.SECRET_KEY));
+
+    const { payload } = await jwtVerify<AdminPayload>(
+      token,
+      new TextEncoder().encode(process.env.SECRET_KEY)
+    );
     const adminId = payload.id;
-    const formdata = await req.formData();
-    const name = formdata.get("name");
-    const slug = formdata.get("slug");
-    const description = formdata.get("description")
-    const price = Number(formdata.get("price"));
-    const category = formdata.get("category")
-    const stock = formdata.get("stock")
     const body = await req.json();
+
     const newProduct = await Product.create({
       ...body,
-      createdBy: adminId
+      createdBy: adminId,
     });
     return NextResponse.json(newProduct, { status: 201 });
   } catch (error) {
-    console.error("Error creating product:", error);
-    return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
+    console.error("err", error);
+    return NextResponse.json({ message: "internal server error" }, { status: 500 });
   }
 }

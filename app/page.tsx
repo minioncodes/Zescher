582d7038f7b862952a78b72b6fee89d7bba5b711
codebyhimanshu <himanshusingh993567@gmail.com>
2025-09-05 17:13:52 +0530

    feat: add Hero component with promotional content and image

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 import FooterNav from "@/components/user/FooterNav";
 import Header from "@/components/user/Header";
+import Hero from "@/components/user/Hero";
+import Products from "@/components/user/Products";
 
 
 export default function Home() {
   return (
   <>
 <Header />
+<Hero />
+
+<Products />
 <FooterNav />
   </>
   );

fcaa2bd4a68e591407860606a8e770cf60138c14
codebyhimanshu <himanshusingh993567@gmail.com>
2025-09-11 16:43:44 +0530

    feat: refactor product fetching and enhance cart functionality with improved error handling

--- app/actions/userActions/user-products.ts
+++ app/actions/userActions/user-products.ts
@@ -1,15 +1,15 @@
-import ProductSchema, { IProduct } from "@/models/admin/ProductSchema"
-import connectDB from "@/lib/mongo"
-    console.log("get products from the action go called");
+"use server";
 
-export async function getProducts() {
-    console.log("inside the get products from the action go called");
-    try {
-        await connectDB()
-        const res = await ProductSchema.find({})
-        console.log("res from the action = ",res);
-        return res as IProduct[]
-    } catch (error) {
-        
-    }
+import ProductSchema, { IProduct } from "@/models/admin/ProductSchema";
+import connectDB from "@/lib/mongo";
+
+export async function getProducts(): Promise<IProduct[]> {
+  try {
+    await connectDB();
+    const res = await ProductSchema.find({});
+    return JSON.parse(JSON.stringify(res)) as IProduct[];
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    return [];
+  }
 }
